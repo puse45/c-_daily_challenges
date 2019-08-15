933bd2dbba1fdf810d13fd56e283b137c653097a
Pius <piusmusyoki45@gmail.com>
2019-08-15 16:41:00 +0300

    Learned iterators and vectors

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iterator>
+#include <vector>
 #include "Animal.h"
 
 void Print(Animal& a){
@@ -57,7 +59,34 @@ int main()
     Print(animal);
      */
 
+    std::vector<int> num2={1,4,5,6};
+    std::vector<int>::iterator itr;
+    // for loop using iterator to get values from vector
+    for(itr = num2.begin();itr< num2.end(); itr++){
+        std::cout << "Vector items " << *itr<<std::endl;
+    }
+    // Advanced in vector using iterator
+    std::vector<int>::iterator itr2 = num2.begin();
+    advance(itr2,2);
+    std::cout<< "Iterator advance " << *itr2 <<std::endl;
 
+    // Next in previous iterator
+    auto itr3 = next(itr2,1);
+    std::cout<< "Iterator next "<<*itr3<<std::endl;
+
+    // Prev function get the previous item from the iterator
+    auto itr4 = prev(itr2, 1);
+    std::cout<< "Iterator previous "<< *itr4 <<std::endl;
+
+
+    // Insert value to existing vector using iterator
+    std::vector<int> num3 = {2,3};
+    auto itr5 = num2.begin();
+    advance(itr5,1);
+    copy(num3.begin(),num3.end(), inserter(num2,itr5));
+    for(auto &i : num2){
+        std::cout<<"Insert values to vector "<<i<<std::endl;
+    }
 
     return 0;
 }
